88fb25c8e37a4175c5627067be33a03fc8f230b0
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-16 23:27:45 +0530

    6. Python, Part 2 > Added python script 17

---
+++ 6. Python, Part 2/Lesson 2 - Strings and Lists, Part 1/17-indexing-2.py
@@ -0,0 +1,32 @@
+# Udacity > Intro to the Programming Nanodegree >
+# Python part 2 > 2. Strings & Lists Part 1 > Section 8: Indexing
+
+'''
+Indexing starts from '0', so the  index count is always one less than
+actual length.
+'''
+# Try this in the interpreter
+>>> "abc"[0]
+'a'
+
+>>> "abc"[2]
+'c'
+
+>>> "waffles"[1]
+'a'
+
+>>> dinner = "falafels"
+>>> dinner[4]
+
+>>> ["a", "b", "c"][2]
+'c'
+
+>>> [1, 2, 3, 4][0]
+1
+
+>>> colors = ["red", "green", "blue"]
+>>> colors[1]
+'green'
+
+>>> countdown = [3, 2, 1, "blastoff!"]
+>>> countdown[3]
