2301bc1c9cb6181d13baba28bc5bc51c4b8c3602
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-16 23:12:15 +0530

    6. Python, Part 2 > Added python script 2

---
+++ 6. Python, Part 2/Lesson 1 - Python at Home/2-input_function.py
@@ -0,0 +1,9 @@
+# Udacity > Intro to the Programming Nanodegree >
+# Python part 2 > 1. Python at Home > Section 5: input function
+
+x = input("Please enter a number.")
+print(x)
+# you call the input function without passing it any arguments
+y = input()
+print(y)
+print("x: ", x, "\ny: ", y)
