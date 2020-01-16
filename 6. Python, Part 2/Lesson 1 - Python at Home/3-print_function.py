68fd50f40487b9a5deb8be852c43c5413fda4fcc
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-16 23:13:12 +0530

    6. Python, Part 2 > Added python script 3

---
+++ 6. Python, Part 2/Lesson 1 - Python at Home/3-print_function.py
@@ -0,0 +1,19 @@
+# Udacity > Intro to the Programming Nanodegree >
+# Python part 2 > 1. Python at Home > Section 6: print function
+
+name = input("What's your name?")
+print("Hello", name, "!")
+# the result will be "Hello <name> !"
+# notice the space between the 'Hello', <name> and '!'. This is unintentional
+# Use the below method for better control
+print("Hello, " + name + "!" + "\nGreetings" + "from" + "udacity")
+print("Hello world!")
+print(2)
+print(2 + 2)
+print('2 + 2')
+print("2 + 2", 2)
+print("2" + "2")
+print(2 * 2)
+print("2" * 2)
+print("2 + 2" * 2)
+print("2 + 2," * 2)
