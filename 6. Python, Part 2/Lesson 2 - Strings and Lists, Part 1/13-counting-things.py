487194af7ec56e59e76b5ed36580a603c766d063
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-16 23:24:18 +0530

    6. Python, Part 2 > Added python script 13

---
+++ 6. Python, Part 2/Lesson 2 - Strings and Lists, Part 1/13-counting-things.py
@@ -0,0 +1,27 @@
+# Udacity > Intro to the Programming Nanodegree >
+# Python part 2 > 2. Strings & Lists Part 1 > Section 6:
+# Counting things (1\2)
+
+print('--------')
+n = 1
+n = n + 1
+print(n)
+
+print('--------')
+x = 0
+x = x + 2
+x = x + 3
+print(x)
+
+print('--------')
+count = 1
+count = count + 1
+count = count + 1
+count = count + 1
+print(count)
+
+print('--------')
+i = 2
+i = i + 2
+i = i + 2
+print(i)
