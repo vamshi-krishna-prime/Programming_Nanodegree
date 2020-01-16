81ce38854ce619754eecb9442a0f4b57ba3aee0a
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-16 23:06:52 +0530

    6. Python, Part 2 > Added python script 1

---
+++ 6. Python, Part 2/Lesson 1 - Python at Home/1-greeting.py
@@ -0,0 +1,6 @@
+# Udacity > Intro to the Programming Nanodegree >
+# Python part 2 > 1. Python at Home > Section 4
+
+name = input("Whats your name?")
+print("Hello,", name, "!")
+print("Nice to meet you!")
