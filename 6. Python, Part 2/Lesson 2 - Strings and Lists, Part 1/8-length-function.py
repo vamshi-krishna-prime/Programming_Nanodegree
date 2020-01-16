4d3699647b1526bb548530b89ff70d390e8103bf
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-16 23:19:00 +0530

    6. Python, Part 2 > Added python script 8

---
+++ 6. Python, Part 2/Lesson 2 - Strings and Lists, Part 1/8-length-function.py
@@ -0,0 +1,23 @@
+# Udacity > Intro to the Programming Nanodegree >
+# Python part 2 > 2. Strings & Lists Part 1 > Section 4: Length
+
+
+print(len("something"))
+print(len("some thing"))
+
+something = "Yay!"
+print(len(something))
+
+word = 'Python scripts'
+print(len(word))
+
+print(len(''))
+print(len(' '))
+print(len(['1', 'a', 3.5]))
+print(len([1, 2, 3]))
+print(len([10, 20, 30]))
+
+my_colors = ["red", "blue"]
+print(len(my_colors))
+
+# len[1, 2, 3] will display error
