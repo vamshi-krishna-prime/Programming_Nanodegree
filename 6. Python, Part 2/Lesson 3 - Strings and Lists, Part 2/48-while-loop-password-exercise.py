0329516756a6cb97b70a591e662d0b63490398f7
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-17 23:33:10 +0530

    6. Python, Part 2 > Added python script 48

---
+++ 6. Python, Part 2/Lesson 3 - Strings and Lists, Part 2/48-while-loop-password-exercise.py
@@ -0,0 +1,13 @@
+# Udacity > Intro to the Programming Nanodegree >
+# Python part 2 > 3. Strings & Lists Part 2 > Section 5:
+# While loops(1/5)
+
+# Password Exercise:
+
+print('Password Exercise:')
+print("HINT: correct password is 'fizzbuzz' ")
+
+password = ''
+while password != 'fizzbuzz':
+    password = input('Please enter the password: ')
+print('Correct passowrd.')
