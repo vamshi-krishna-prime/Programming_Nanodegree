5f42ea175311f467cea75a17847cd537753cae4f
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-17 23:04:48 +0530

    6. Python, Part 2 > Added python script 38

---
+++ 6. Python, Part 2/Lesson 2 - Strings and Lists, Part 1/38-boolean-values.py
@@ -0,0 +1,45 @@
+# Udacity > Intro to the Programming Nanodegree >
+# Python part 2 > 2. Strings & Lists Part 1 > Section 22:
+# Boolean values
+
+# Try them in interpreter
+
+>>> True and True
+True
+
+>>> True and False
+False
+
+>>> False and False
+False
+
+>>> True or True
+True
+
+>>> True or False
+True
+
+>>> False or False
+False
+
+>>> True and True
+True
+
+>>> 2 == 2 and 5 == 5
+True
+
+>>> n = 5
+>>> n > 0 and n < 10
+True
+
+>>> n = 100
+>>> n > 0 and n < 10
+False
+
+>>> s = "foo"
+>>> len(s) < 5 or len(s) > 10
+True
+
+>>> s = "foobar"
+>>> len(s) < 5 or len(s) > 10
+False
