a815878850c0c5ed4e8606ca5425c384c23040ab
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-17 23:31:11 +0530

    6. Python, Part 2 > Added python script 46

---
+++ 6. Python, Part 2/Lesson 3 - Strings and Lists, Part 2/46-while-loop.py
@@ -0,0 +1,18 @@
+# Udacity > Intro to the Programming Nanodegree >
+# Python part 2 > 3. Strings & Lists Part 2 > Section 5:
+# While loops(1/5)
+
+# While loop 1
+print('\nWhile loop 1:')
+n = 1
+while n < 3:
+    print(n)
+    n += 1
+
+
+# While loop 2
+print('\nWhile loop 2:')
+n = 0
+while n < 3:
+    print(n)
+    n += 1
