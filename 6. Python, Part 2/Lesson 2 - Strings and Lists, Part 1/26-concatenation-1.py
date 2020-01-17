4cd4ac1c7248d4bb594573df0b461b9421bee837
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-17 15:10:09 +0530

    6. Python, Part 2 > Added python script 26

---
+++ 6. Python, Part 2/Lesson 2 - Strings and Lists, Part 1/26-concatenation-1.py
@@ -0,0 +1,28 @@
+# Udacity > Intro to the Programming Nanodegree >
+# Python part 2 > 2. Strings & Lists Part 1 > Section 14: Concatenation(1/2)
+
+# below are executed in python interpreter
+
+>>> '1' + '1'
+'11'
+
+>>> '1' + 1 
+TypeError
+
+>>> '1' + ' ' + '1'
+'1 1'
+
+>>> s = '1'
+>>> s + s + '1'
+'111'
+
+>>> 1.2 + 2.3
+>>> 'Appple' + 'pie'
+>>> fruit = 'strawberry'
+>>> fruit + 'pie'
+>>> fruit + pie
+>>> 'This' + 'is' + 'very' + 'silly'
+>>> red_brother = 'Mario'
+>>> green_brother = 'Luigi'
+>>> red_brother + 'and ' + green_brother
+>>> 4 + 'oranges'
