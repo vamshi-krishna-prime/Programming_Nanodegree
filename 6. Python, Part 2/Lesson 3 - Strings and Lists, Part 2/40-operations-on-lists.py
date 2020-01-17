f5f6d4b3a2f2d849605744675730e265870253f4
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-17 23:25:28 +0530

    6. Python, Part 2 > Added python script 40

---
+++ 6. Python, Part 2/Lesson 3 - Strings and Lists, Part 2/40-operations-on-lists.py
@@ -0,0 +1,30 @@
+# Udacity > Intro to the Programming Nanodegree >
+# Python part 2 > 3. Strings & Lists Part 2 > Section 1:
+# Operations on lists
+
+'''
+Here are some things that actually work on all sequence types:
+
+1. the indexing operation
+2. the slicing operation
+3. the len function
+
+To get a feeling for how these operations work on lists,
+here are some examples you can try in interpreter:
+'''
+
+>>> my_list = ["a", "b", "c", "d"]
+>>> my_list[1]
+'b'
+
+>>> my_list[0:2]
+['a', 'b']
+
+>>> len(my_list)
+4
+
+>>> my_list[3] == "d"
+True
+
+>>> len(['Queen', 'rules'])
+2
