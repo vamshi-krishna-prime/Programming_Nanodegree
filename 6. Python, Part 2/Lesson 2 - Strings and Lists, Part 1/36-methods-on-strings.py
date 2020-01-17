2c8d5d6baf3643566da95525a4d2d3698ea171c0
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-17 22:54:26 +0530

    6. Python, Part 2 > Added python script 36

---
+++ 6. Python, Part 2/Lesson 2 - Strings and Lists, Part 1/36-methods-on-strings.py
@@ -0,0 +1,19 @@
+# Udacity > Intro to the Programming Nanodegree >
+# Python part 2 > 2. Strings & Lists Part 1 > Section 21:
+# Methods on strings
+
+# There are many methods on strings. Some of them are: (Try them in interpreter)
+
+>>> "banana".startswith("ban")
+True
+>>> "bonobo".startswith("ban")
+False
+>>> "bonobo".endswith("ban")
+False
+>>> num = 34
+>>> num.isdigit()
+True
+>>> "heat".islower()
+True
+>>> "   ".islower()
+True
