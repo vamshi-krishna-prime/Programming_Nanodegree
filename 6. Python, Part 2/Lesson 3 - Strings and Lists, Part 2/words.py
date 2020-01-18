9c37f45e4d89b339b20ae134ff590b35e4aabb4b
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-18 23:33:14 +0530

    6. Python, Part 2 > Added support script for 70

---
+++ 6. Python, Part 2/Lesson 3 - Strings and Lists, Part 2/words.py
@@ -0,0 +1,27 @@
+# Udacity > Intro to the Programming Nanodegree >
+# Python part 2 > 3. Strings & Lists Part 2 > Section 19:
+# Silly sentences
+
+# Supporting script for 'random-silly-sentences-loop' script
+
+nouns = ['apple', 'ball', 'cat', 'dog', 'elephant',
+         'fish', 'goat', 'house', 'iceberg', 'jackal',
+         'king', 'llama', 'monkey', 'nurse', 'octopus',
+         'pie', 'queen', 'robot', 'snake', 'tofu',
+         'unicorn', 'vampire', 'wumpus', 'x-ray', 'yak',
+         'zebra']
+
+verbs = ['ate', 'bit', 'caught', 'dropped', 'explained',
+         'fed', 'grabbed', 'hacked', 'inked', 'jumped',
+         'knitted', 'loved', 'made', 'nosed', 'oiled',
+         'puffed', 'quit', 'rushed', 'stung', 'trapped',
+         'uplifted', 'valued', 'wanted']
+
+templates = [
+        'Waiter! I found a {{noun}} in my {{noun}}!',
+        'The {{noun}} {{verb}} the {{noun}}.',
+        'If you {{verb}} the {{noun}}, '
+        'the {{noun}} will get you.',
+        "Let's go: the {{noun}} is {{verb}}.",
+        'Colorless green {{noun}}s {{verb}} furiously.'
+]
